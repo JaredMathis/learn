05a8586d9f18bf6b4cec5652bcb3570f5293aedc
JaredMathis <jared.mathis@native.consulting>
2023-02-05 11:10:21 -0500

    function_auto semantic_superset

--- src/semantic/superset.mjs
+++ src/semantic/superset.mjs
@@ -1,6 +1,11 @@
+import { list_single } from './../../node_modules/m00/src/list/single.mjs';
+import { semantic_binary } from './binary.mjs';
 export function semantic_superset(parsed) {
     if (parsed.length === 1) {
         return list_single(parsed);
     }
-    
+    let inner = {};
+    if (semantic_binary(parsed, ['not'], inner)) {
+        return inner.p0 + ' ' + inner.p1;
+    }
 }
