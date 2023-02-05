8a90724440f3af929cd4c16e9c76e2c6d01ab678
JaredMathis <jared.mathis@native.consulting>
2023-02-05 04:31:22 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,5 +1,7 @@
+import { log } from './../node_modules/m00/src/log.mjs';
 import { string_split_space } from './../node_modules/m00/src/string/split/space.mjs';
 export function sb() {
     let input = `Jared is human`;
     let tokens = string_split_space(input);
+    console.log(tokens);
 }
