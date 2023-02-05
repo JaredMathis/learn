dcfec91a960d23ebefb7cccf02fd0ef9d76d4d79
JaredMathis <jared.mathis@native.consulting>
2023-02-05 06:50:10 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,7 +1,8 @@
+import { json_to } from './../node_modules/m00/src/json/to.mjs';
 import { log } from './../node_modules/m00/src/log.mjs';
 import parser from 'paren';
 export function sb() {
     let parenthesized = `( ( Jared ) is ( human ) )`;
     let parsed = parser(parenthesized, ' ');
-    console.log({ parsed });
+    console.log(json_to(parsed));
 }
