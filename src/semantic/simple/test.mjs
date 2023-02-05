1822a6188fdb214d64219fa14f882c2aabd8c352
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:13:17 -0500

    function_auto_run semantic_simple_test

--- src/semantic/simple/test.mjs
+++ src/semantic/simple/test.mjs
@@ -1,7 +1,8 @@
+import { assert } from './../../../node_modules/m00/src/assert.mjs';
 import { semantic_parse } from './../parse.mjs';
 import { semantic_is } from './../is.mjs';
 export function semantic_simple_test() {
     let parsed = semantic_parse(`( Jared ) is ( human )`);
     let context = {};
-    semantic_is(parsed, context);
+    assert(semantic_is(parsed, context));
 }
