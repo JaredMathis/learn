312c119e3c54a0f51ca525f13de679e9a71adc0a
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:30:07 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,3 +1,4 @@
+import { log } from './../node_modules/m00/src/log.mjs';
 import { list_slice } from './../node_modules/m00/src/list/slice.mjs';
 import { list_index_of } from './../node_modules/m00/src/list/index/of.mjs';
 import { string_split_space } from './../node_modules/m00/src/string/split/space.mjs';
@@ -7,4 +8,8 @@ export function sb() {
     let index = list_index_of(tokens, 'is');
     let before = list_slice(tokens, 0, index);
     let after = list_slice(tokens, index + 1, tokens.length);
+    console.log({
+        before,
+        after
+    });
 }
