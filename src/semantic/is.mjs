fe7bd665683cf869a50e80ce0fad1a4b97b24041
JaredMathis <jared.mathis@native.consulting>
2023-02-05 06:57:29 -0500

    function_auto semantic_is

--- src/semantic/is.mjs
+++ src/semantic/is.mjs
@@ -1,2 +1,17 @@
-export function semantic_is() {
+import { arguments_assert } from './../../node_modules/m00/src/arguments/assert.mjs';
+import { list_is } from './../../node_modules/m00/src/list/is.mjs';
+export function semantic_is(parsed) {
+    arguments_assert(arguments, list_is);
+    if (parsed.length !== 3) {
+        return false;
+    }
+    if (!list_is(parsed[0])) {
+        return false;
+    }
+    if (parsed[1] !== 'is') {
+        return false;
+    }
+    if (!list_is(parsed[2])) {
+        return false;
+    }
 }
