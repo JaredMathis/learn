7b6fe6d675335607540481967355cdb4a7a687cb
JaredMathis <jared.mathis@native.consulting>
2023-02-05 10:10:35 -0500

    function_auto semantic_and

--- src/semantic/and.mjs
+++ src/semantic/and.mjs
@@ -1,2 +1,14 @@
-export function semantic_and() {
+import { semantic_ternary } from './ternary.mjs';
+import { for_each } from './../../node_modules/m00/src/for/each.mjs';
+export function semantic_and(parsed, if_parse) {
+    let context = {};
+    if (semantic_ternary(parsed, ['and'], context)) {
+        for_each([
+            '0',
+            '2'
+        ], i => {
+            if_parse(inner['p' + i]);
+        });
+        return true;
+    }
 }
