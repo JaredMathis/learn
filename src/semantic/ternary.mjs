64519d6e24d7dfe24f2e6347f807fee7f4f1d48d
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:45:04 -0500

    function_auto semantic_ternary

--- src/semantic/ternary.mjs
+++ src/semantic/ternary.mjs
@@ -1,2 +1,23 @@
-export function semantic_ternary() {
+import { list_is } from './../../node_modules/m00/src/list/is.mjs';
+import { merge } from './../../node_modules/m00/src/merge.mjs';
+export function semantic_ternary(parsed, middle, context) {
+    if (parsed.length !== 3) {
+        return false;
+    }
+    let p0 = parsed[0];
+    if (!list_is(p0)) {
+        return false;
+    }
+    if (parsed[1] !== middle) {
+        return false;
+    }
+    let p2 = parsed[2];
+    if (!list_is(p2)) {
+        return false;
+    }
+    merge(context, {
+        p0,
+        p2
+    });
+    return true;
 }
