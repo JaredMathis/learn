8dbb91262a57ed12a6c85ae4473f7cae6875dae7
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:40:17 -0500

    noop progress

--- src/semantic/is.mjs
+++ src/semantic/is.mjs
@@ -8,14 +8,14 @@ export function semantic_is(parsed, context) {
     if (parsed.length !== 3) {
         return false;
     }
-    const p0 = parsed[0];
+    let p0 = parsed[0];
     if (!list_is(p0)) {
         return false;
     }
     if (parsed[1] !== 'is') {
         return false;
     }
-    const p2 = parsed[2];
+    let p2 = parsed[2];
     if (!list_is(p2)) {
         return false;
     }
