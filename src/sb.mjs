3a03f7230a2a5bda9f875fa64695e48900ad166c
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:29:55 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,7 +1,10 @@
+import { list_slice } from './../node_modules/m00/src/list/slice.mjs';
 import { list_index_of } from './../node_modules/m00/src/list/index/of.mjs';
 import { string_split_space } from './../node_modules/m00/src/string/split/space.mjs';
 export function sb() {
     let input = `Jared is human`;
     let tokens = string_split_space(input);
-    let is = list_index_of(tokens, 'is');
+    let index = list_index_of(tokens, 'is');
+    let before = list_slice(tokens, 0, index);
+    let after = list_slice(tokens, index + 1, tokens.length);
 }
