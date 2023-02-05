c006c23efc8d01565904109f3dabe61f12eb2fa8
JaredMathis <jared.mathis@native.consulting>
2023-02-05 08:12:47 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,7 +1,2 @@
-import { semantic_parse } from './semantic/parse.mjs';
-import { semantic_is } from './semantic/is.mjs';
 export function sb() {
-    let parsed = semantic_parse(`( Jared ) is ( human )`);
-    let context = {};
-    semantic_is(parsed, context);
 }
--- src/semantic/simple/test.mjs
+++ src/semantic/simple/test.mjs
@@ -1,2 +1,5 @@
 export function semantic_simple_test() {
+    let parsed = semantic_parse(`( Jared ) is ( human )`);
+    let context = {};
+    semantic_is(parsed, context);
 }
