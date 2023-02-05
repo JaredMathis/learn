713bf9ebc405d2d97bd8236783a0f943a0982110
JaredMathis <jared.mathis@native.consulting>
2023-02-05 10:19:27 -0500

    function_auto semantic_is

--- src/semantic/is.mjs
+++ src/semantic/is.mjs
@@ -1,9 +1,8 @@
+import { semantic_context_subset_for_each } from './context/subset/for/each.mjs';
 import { list_add } from './../../node_modules/m00/src/list/add.mjs';
-import { for_each } from './../../node_modules/m00/src/for/each.mjs';
 import { semantic_and } from './and.mjs';
 import { list_single } from './../../node_modules/m00/src/list/single.mjs';
 import { defined_is } from './../../node_modules/m00/src/defined/is.mjs';
-import { semantic_context_subset } from './context/subset.mjs';
 import { arguments_assert } from './../../node_modules/m00/src/arguments/assert.mjs';
 import { list_is } from './../../node_modules/m00/src/list/is.mjs';
 import { semantic_ternary } from './ternary.mjs';
@@ -25,11 +24,11 @@ export function semantic_is(parsed, context) {
     }
     if (p2.length === 1) {
         p2 = list_single(p2);
-            semantic_context_subset_for_each(context, subjects, p2);
+        semantic_context_subset_for_each(context, subjects, p2);
         return true;
     }
     if (semantic_and(p2, part => {
-        semantic_context_subset_for_each(context, subjects, list_single(part));
+            semantic_context_subset_for_each(context, subjects, list_single(part));
         })) {
         return true;
     }
