39f880a2475ea4aad93e941cb9a8a147f6456d1c
JaredMathis <jared.mathis@native.consulting>
2023-02-05 10:18:48 -0500

    function_auto semantic_context_subset_for_each

--- src/semantic/context/subset/for/each.mjs
+++ src/semantic/context/subset/for/each.mjs
@@ -1,7 +1,12 @@
+import { arguments_assert } from './../../../../../node_modules/m00/src/arguments/assert.mjs';
+import { defined_is } from './../../../../../node_modules/m00/src/defined/is.mjs';
+import { list_is } from './../../../../../node_modules/m00/src/list/is.mjs';
+import { string_is } from './../../../../../node_modules/m00/src/string/is.mjs';
 import { for_each } from './../../../../../node_modules/m00/src/for/each.mjs';
 import { semantic_context_subset } from './../../subset.mjs';
 export function semantic_context_subset_for_each(context, subsets, superset) {
-    for_each(subjects, s => {
-        semantic_context_subset(context, s, p2);
+    arguments_assert(arguments, defined_is, list_is, string_is);
+    for_each(subsets, s => {
+        semantic_context_subset(context, s, superset);
     });
 }
