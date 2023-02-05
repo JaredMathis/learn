c32223ef98ed6411488e147d1ac7bd6eb25baf36
JaredMathis <jared.mathis@native.consulting>
2023-02-05 06:57:43 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,8 +1,8 @@
-import { json_to } from './../node_modules/m00/src/json/to.mjs';
+import { semantic_is } from './semantic/is.mjs';
 import { log } from './../node_modules/m00/src/log.mjs';
 import parser from 'paren';
 export function sb() {
     let parenthesized = `( Jared ) is ( human )`;
     let parsed = parser(parenthesized, ' ');
-    log(json_to(parsed));
+    log(semantic_is(parsed));
 }
