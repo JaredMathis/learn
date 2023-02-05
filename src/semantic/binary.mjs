481058e9815db05081f0237ba47710e8d2940da1
JaredMathis <jared.mathis@native.consulting>
2023-02-05 11:07:58 -0500

    function_auto semantic_binary

--- src/semantic/binary.mjs
+++ src/semantic/binary.mjs
@@ -1,2 +1,23 @@
-export function semantic_binary() {
+import { arguments_assert } from './../../node_modules/m00/src/arguments/assert.mjs';
+import { defined_is } from './../../node_modules/m00/src/defined/is.mjs';
+import { list_is } from './../../node_modules/m00/src/list/is.mjs';
+import { merge } from './../../node_modules/m00/src/merge.mjs';
+export function semantic_binary(parsed, prefix, context) {
+    arguments_assert(arguments, list_is, list_is, defined_is);
+    if (parsed.length !== 2) {
+        return false;
+    }
+    let p0 = parsed[0];
+    if (!prefix.includes(p0)) {
+        return false;
+    }
+    let p1 = parsed[1];
+    if (!list_is(p1)) {
+        return false;
+    }
+    merge(context, {
+        p0,
+        p1
+    });
+    return true;
 }
