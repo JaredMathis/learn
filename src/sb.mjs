007fee8604194e9017613c693c8cccead4f598b6
JaredMathis <jared.mathis@native.consulting>
2023-02-05 06:43:42 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,16 +1,7 @@
-import { list_slice_end_to } from './../node_modules/m00/src/list/slice/end/to.mjs';
-import { list_slice_begin_from } from './../node_modules/m00/src/list/slice/begin/from.mjs';
 import { log } from './../node_modules/m00/src/log.mjs';
-import { list_index_of } from './../node_modules/m00/src/list/index/of.mjs';
-import { string_split_space } from './../node_modules/m00/src/string/split/space.mjs';
+import parser from 'paren';
 export function sb() {
-    let input = `Jared is human`;
-    let tokens = string_split_space(input);
-    let index = list_index_of(tokens, 'is');
-    let before = list_slice_begin_from(tokens, index);
-    let after = list_slice_end_to(tokens, index + 1);
-    console.log({
-        before,
-        after
-    });
+    let input = `( Jared ) is ( human )`;
+    let parsed = parser(input, ' ');
+    console.log({ parsed });
 }
