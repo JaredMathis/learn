7e22a7d8b4101a86813897ab06a8ec01c262391b
JaredMathis <jared.mathis@native.consulting>
2023-02-05 10:18:18 -0500

    function_auto semantic_context_subset_for_each

--- src/semantic/context/subset/for/each.mjs
+++ src/semantic/context/subset/for/each.mjs
@@ -1,2 +1,7 @@
-export function semantic_context_subset_for_each() {
+import { for_each } from './../../../../../node_modules/m00/src/for/each.mjs';
+import { semantic_context_subset } from './../../subset.mjs';
+export function semantic_context_subset_for_each(context, subsets, superset) {
+    for_each(subjects, s => {
+        semantic_context_subset(context, s, p2);
+    });
 }
