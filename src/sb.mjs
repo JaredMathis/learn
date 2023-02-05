caff2d7dbd99b081bed9af55d00d3d45c3254756
JaredMathis <jared.mathis@native.consulting>
2023-02-05 05:18:30 -0500

    function_auto_run sb

--- src/sb.mjs
+++ src/sb.mjs
@@ -1,7 +1,7 @@
-import { log } from './../node_modules/m00/src/log.mjs';
+import { list_index_of } from './../node_modules/m00/src/list/index/of.mjs';
 import { string_split_space } from './../node_modules/m00/src/string/split/space.mjs';
 export function sb() {
     let input = `Jared is human`;
     let tokens = string_split_space(input);
-    console.log(tokens);
+    let is = list_index_of(tokens, 'is');
 }
