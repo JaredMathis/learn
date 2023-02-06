10501cba04b00ae7962f61ef50791d2968d8a947
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:36:42 -0500

    function_auto semantic_meaning

--- src/semantic/meaning.mjs
+++ src/semantic/meaning.mjs
@@ -1,2 +1,6 @@
-export function semantic_meaning() {
+import { semantic_is } from './is.mjs';
+export function semantic_meaning(parsed, context) {
+    let copy = json_copy(context);
+    if (semantic_is(parsed, copy)) {
+    }
 }
