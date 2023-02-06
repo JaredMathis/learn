89bd5a67a223acfb1e31fb2e1dd712a2d46a0d7d
JaredMathis <jared.mathis@native.consulting>
2023-02-06 03:38:23 -0500

    function_auto_run semantic_simple_7_test

--- src/semantic/simple/7/test.mjs
+++ src/semantic/simple/7/test.mjs
@@ -1,2 +1,10 @@
+import { assert } from './../../../../node_modules/m00/src/assert.mjs';
+import { semantic_parse_meaning } from './../../parse/meaning.mjs';
+import { assert_throws } from './../../../../node_modules/m00/src/assert/throws.mjs';
 export function semantic_simple_7_test() {
+    let context = {};
+    assert(semantic_parse_meaning(`( jordan ) is ( sexy )`, context));
+    assert(semantic_parse_meaning(`( sexy ) is ( attractive )`, context));
+    assert(semantic_parse_meaning(`( attractive ) is ( good )`, context));
+    assert_throws(() => semantic_parse_meaning(`( jordan ) is ( not ( attractive ) )`, context));
 }
