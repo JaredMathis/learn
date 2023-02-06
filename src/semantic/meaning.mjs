0f635f9c08adf0960f817787315e671e3674b8fe
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:50:38 -0500

    function_auto semantic_meaning

--- src/semantic/meaning.mjs
+++ src/semantic/meaning.mjs
@@ -1,9 +1,10 @@
+import { properties_replace } from './../../node_modules/m00/src/properties/replace.mjs';
 import { json_copy } from './../../node_modules/m00/src/json/copy.mjs';
 import { semantic_is } from './is.mjs';
 export function semantic_meaning(parsed, context) {
     let copy = json_copy(context);
     if (semantic_is(parsed, copy)) {
-        object_replace(context, copy);
+        properties_replace(context, copy);
         return true;
     }
     return false;
