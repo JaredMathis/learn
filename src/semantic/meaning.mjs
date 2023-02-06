4182c2671c13cd9eeb224b0ba8d11125e5ca586c
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:38:37 -0500

    function_auto semantic_meaning

--- src/semantic/meaning.mjs
+++ src/semantic/meaning.mjs
@@ -1,3 +1,4 @@
+import { json_copy } from './../../node_modules/m00/src/json/copy.mjs';
 import { semantic_is } from './is.mjs';
 export function semantic_meaning(parsed, context) {
     let copy = json_copy(context);
