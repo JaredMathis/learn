4be11461b21d5254278f2eebb7e03a88168a66fa
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:47:45 -0500

    function_auto_tests semantic_simple_6_test

--- src/semantic/simple/6/test.mjs
+++ src/semantic/simple/6/test.mjs
@@ -3,6 +3,7 @@ import { assert } from './../../../../node_modules/m00/src/assert.mjs';
 import { assert_throws } from './../../../../node_modules/m00/src/assert/throws.mjs';
 export function semantic_simple_6_test() {
     let context = {};
-    assert(semantic_parse_meaning(`( jared ) is ( not ( bad ) )`, context));
+    assert(semantic_parse_meaning(`( jared ) is ( good )`, context));
+    assert(semantic_parse_meaning(`( good ) is ( not ( bad ) )`, context));
     assert_throws(() => semantic_parse_meaning(`( jared ) is ( bad )`), context);
 }
