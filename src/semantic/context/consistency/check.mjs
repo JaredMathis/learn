342729372cf61288c7f9872f240e1beebc8ff48a
JaredMathis <jared.mathis@native.consulting>
2023-02-05 21:35:26 -0500

    tests

--- src/semantic/context/consistency/check.mjs
+++ src/semantic/context/consistency/check.mjs
@@ -3,15 +3,22 @@ import { list_single } from './../../../../node_modules/m00/src/list/single.mjs'
 import { property_has } from './../../../../node_modules/m00/src/property/has.mjs';
 import { semantic_parse } from './../../parse.mjs';
 import { for_each_key } from './../../../../node_modules/m00/src/for/each/key.mjs';
+import { list_add } from 'm00/src/list/add.mjs';
 export function semantic_context_consistency_check(context) {
+    let errors = [];
     for_each_key(context, set_name => {
         if (set_name.startsWith('not (')) {
             let parsed = semantic_parse(set_name);
             assert(parsed.length === 2);
-            let negated = list_single(parsed[1]);
-            if (property_has(context, negated)) {
-                
+            let unnegated = list_single(parsed[1]);
+            if (property_has(context, unnegated)) {
+                for_each_key(context[set_name], subset_name => {
+                    if (context[unnegated][subset_name]) {
+                        list_add(errors, `${subset_name} cannot be ${unnegated} and not ${unnegated} at the same time`)
+                    }
+                }) 
             }
         }
     });
+    return errors;
 }
