f95f3fbc8e410fb840a4fb0933a624bebd115ac6
JaredMathis <jared.mathis@native.consulting>
2023-02-06 02:42:11 -0500

    function_auto semantic_parse_meaning

--- src/semantic/parse/meaning.mjs
+++ src/semantic/parse/meaning.mjs
@@ -1,2 +1,6 @@
-export function semantic_parse_meaning() {
+import { semantic_parse } from './../parse.mjs';
+import { semantic_meaning } from './../meaning.mjs';
+export function semantic_parse_meaning(input, context) {
+    let parsed = semantic_parse(input);
+    return semantic_meaning(parsed, context);
 }
