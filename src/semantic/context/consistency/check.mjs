6f4c89f495d7a9a0b4de150621d4a7b049c363d2
JaredMathis <jared.mathis@native.consulting>
2023-02-05 21:18:11 -0500

    function_auto semantic_context_consistency_check

--- src/semantic/context/consistency/check.mjs
+++ src/semantic/context/consistency/check.mjs
@@ -1,2 +1,5 @@
-export function semantic_context_consistency_check() {
+import { for_each_key } from './../../../../node_modules/m00/src/for/each/key.mjs';
+export function semantic_context_consistency_check(context) {
+    for_each_key(context, set => {
+    });
 }
